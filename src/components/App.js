137fb0cb37aa71a8bd40572dfa9a8f64adad673d
Gokul Kramnik <ramakrishnan.go@husky.neu.edu>
2019-06-22 07:55:14 -0700

    Adding Redux Support
    Cleaning up unnecessary files and adding support for Redux actions and reducers.

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { selectSong } from '../actions';
+
+const App = () => {
+
+    return(
+        <div>
+            React Songs App
+        </div>
+    );
+}
+
+export default App;
