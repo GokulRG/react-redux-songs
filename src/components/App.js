45b8978500081c53e1b67a88137345091d34ef2f
Gokul Kramnik <ramakrishnan.go@husky.neu.edu>
2019-06-25 03:26:40 -0700

    Styling Changes
    Adding small styling changes to fine-tune the look and feel.

--- src/components/App.js
+++ src/components/App.js
@@ -7,10 +7,10 @@ const App = () => {
     return(
         <div className="ui container grid">
             <div className="ui row">
-                <div className="column eight wide">
+                <div style={{marginTop:'10px'}} className="column eight wide">
                     <SongList />
                 </div>
-                <div className="column eight wide">
+                <div style={{marginTop:'10px'}} className="column eight wide">
                     <SongDetail />
                 </div>
             </div>
